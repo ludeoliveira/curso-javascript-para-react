8eb3c82ea826bec0cbe0a8742ed2aa43439963b5
luludeolive <lucianaoliveirabacelar@gmail.com>
2021-11-24 11:08:40 -0300

    Initialize repo

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,12 @@
+//JSX
+import "./style.css"
+
+function CardGame() {
+    return `
+        <article class= "card-game">
+            <img src ="./src/images/cosmica-logo.png" alt="Logo da Cósmica Press" width= "110">
+        </article>
+    `
+}
+
+export default CardGame;
