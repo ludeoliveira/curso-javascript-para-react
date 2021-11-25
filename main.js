ffd4b1937892b9fe06963d3337d272445bd82e8a
luludeolive <lucianaoliveirabacelar@gmail.com>
2021-11-25 10:29:19 -0300

    update styles

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 import "./src/styles/settings/colors.css"
 import "./src/styles/generic/reset.css"
-import "./src/styles/elements/base.css"
- 
+import "./src/styles/elements/base.css" 
 import BoardGame from "./src/objects/BoardGame"
 
 const $root = document.querySelector("#root")
-const $htmlBoardGame = BoardGame(6 )
+const $htmlBoardGame = BoardGame(6)
 
 
 $root.insertAdjacentHTML("beforeend", $htmlBoardGame) 
